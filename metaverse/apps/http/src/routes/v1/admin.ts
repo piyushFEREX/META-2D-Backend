7b0bb9fb3a0a23056e99e83bfea062f2b5e9070c
piyush-Remote <930piy@gmail.com>
2024-12-21 16:24:45 +0000

    forgot to pus the backend

---
+++ metaverse/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+export const adminRouter = Router()
+
+
+adminRouter.post('/element',(req,res)=>{
+
+})
+adminRouter.put('/:elementId',(req,res)=>{
+
+})
+adminRouter.post('/map',(req,res)=>{
+
+})
